17073a6d0debc4943c831c8d325c7d6013f0be7e
Jeffrey Tvedt <jeffreytvedt5@gmail.com>
2025-01-09 22:51:07 -0500

    doc: added javadoc comments for State and StateMachine

--- src/main/java/org/syofrc/syolib/StateMachine.java
+++ src/main/java/org/syofrc/syolib/StateMachine.java
@@ -1,11 +1,23 @@
 package org.syofrc.syolib;
 
+/**
+ * Finite State Machine (FSM) that operates on a particular type of object
+ * @param <T> The type of object the FSM is compatible with
+ */
 public class StateMachine<T> {
     private State<T> wantedState;
     private State<T> currentState;
 
+    /**
+     * Sets the desired state for the state machine to accept transition to
+     * @param state Desired state for the FSM
+     */
     public void setWanted(State<T> state) { wantedState = state; }
     
+    /**
+     * Process the current state using the provided object
+     * @param object Object for the FSM to work on
+     */
     public void accept(T object) {
         if (currentState != wantedState) {
             State<T> previousState = currentState;
